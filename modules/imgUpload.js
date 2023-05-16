6e2711508104545f322ad747d66c3d31ed27ae01
reza-erfit <muhammad.reza.erfit-2020@ftmm.unair.ac.id>
2023-05-16 10:07:16 +0700

    Commit File

--- modules/imgUpload.js
+++ modules/imgUpload.js
@@ -8,12 +8,12 @@ const pathKey = path.resolve('./serviceaccountkey.json')
 
 // TODO: Sesuaikan konfigurasi Storage
 const gcs = new Storage({
-    projectId: 'project_id_Anda',
+    projectId: 'submission-mgce-rezaerfit25',
     keyFilename: pathKey
 })
 
 // TODO: Tambahkan nama bucket yang digunakan
-const bucketName = 'nama_GCS_bucket_Anda'
+const bucketName = 'dicoding-submission-2511'
 const bucket = gcs.bucket(bucketName)
 
 function getPublicUrl(filename) {
--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.101.173.251',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'my_database',
+    password: 'sukses25'
 })
 
 router.get("/dashboard", (req, res) => {
